22d2057c7152564d9d58215e322b12b41f91b9a2
tyndria <tyndria23@gmail.com>
2016-04-26 15:35:50 +0200

    Add screenshots to img/

---
+++ webchatapp/src/main/bsu.fpmi.servlets/VarsServlet.java
@@ -0,0 +1,29 @@
+package main.bsu.fpmi.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetEncoder;
+
+
+/** execute doGet
+ * Created by Антонина on 25.04.16.
+ */
+@WebServlet(value = "/vars")
+
+public class VarsServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        final String[] vars = {"JAVA_HOME", "M2_HOME", "CATALINA_HOME","USERNAME", "PATH"};
+        for (String var : vars) {
+            resp.setContentType("text/html;charset=Windows-1251");
+            resp.getWriter().println(String.format("%s=%s", var, System.getenv(var)));
+            //resp.getOutputStream().println(String.format("%s=%s", var, System.getenv(var)));
+        }
+    }
+}
