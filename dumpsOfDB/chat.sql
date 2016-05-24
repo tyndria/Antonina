97bd4b81a3835b964bdf5cec476d46aa9a28027d
tyndria <tyndria23@gmail.com>
2016-05-24 23:15:15 +0200

    change db a little bit

--- dumpsOfDB/chat.sql
+++ dumpsOfDB/chat.sql
@@ -27,12 +27,12 @@ DROP TABLE IF EXISTS `messages`;
 CREATE TABLE `messages` (
   `idMessages` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `text` varchar(45) NOT NULL,
-  `date` date DEFAULT NULL,
+  `dateText` date DEFAULT NULL,
   `idUsers` int(11) DEFAULT NULL,
   PRIMARY KEY (`idMessages`),
   KEY `user-message_idx` (`idUsers`),
   CONSTRAINT `user-message` FOREIGN KEY (`idUsers`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=cp1251;
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=cp1251;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -41,7 +41,7 @@ CREATE TABLE `messages` (
 
 LOCK TABLES `messages` WRITE;
 /*!40000 ALTER TABLE `messages` DISABLE KEYS */;
-INSERT INTO `messages` VALUES (1,'hi, guys!','2016-05-09',1),(2,'hello, tonya)','2016-05-09',2),(3,'how are you?)','2016-05-09',1),(4,'ooo, are you here??','2016-05-09',3),(5,'great! ','2016-05-09',3),(6,'what\'s up, anton?','2016-05-09',1),(7,'don\'t you know?','2016-05-10',8),(8,'he\'s got a job!','2016-05-10',7),(9,'that\'s true!)','2016-05-09',8),(10,'my congratulations! well done!','2016-05-11',6),(11,'hello!)','2016-05-24',6),(12,'where are you??','2016-05-24',6),(13,'guys..?','2016-05-24',6),(14,'i\'m lonely.................','2016-05-24',6),(15,'hi, bulbash!','2016-05-24',8),(16,'you are not lonely!','2016-05-24',8),(17,'don\'t be worry!','2016-05-24',8),(18,'be happy!','2016-05-24',8);
+INSERT INTO `messages` VALUES (1,'hi, guys!','2016-05-09',1),(2,'hello, tonya)','2016-05-09',2),(3,'how are you?)','2016-05-09',1),(4,'ooo, are you here??','2016-05-09',3),(5,'great! ','2016-05-09',3),(6,'what\'s up, anton?','2016-05-09',1),(7,'don\'t you know?','2016-05-10',8),(8,'he\'s got a job!','2016-05-10',7),(9,'that\'s true!)','2016-05-09',8),(10,'my congratulations! well done!','2016-05-11',6),(11,'hello!)','2016-05-24',6),(12,'where are you??','2016-05-24',6),(13,'guys..?','2016-05-24',6),(14,'i\'m lonely.................','2016-05-24',6),(15,'hi, bulbash!','2016-05-24',8),(16,'you are not lonely!','2016-05-24',8),(17,'don\'t be worry!','2016-05-24',8),(18,'be happy!','2016-05-24',8),(19,'arejyou','2016-05-24',8);
 /*!40000 ALTER TABLE `messages` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -79,4 +79,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-05-24 16:53:31
+-- Dump completed on 2016-05-24 23:13:51
